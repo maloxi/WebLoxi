02ae962a38e851cc548e1e49bd35983805c7a32f
XeonTruc <XeonTruc@XEONTRUC-PC>
2017-02-26 07:44:15 +0100

    Add project files.

---
+++ WebApplication4/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication4.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="Label1" runat="server" Text="Jmeno"></asp:Label>
+       
+
+           <br>
+        <asp:TextBox ID="TextBox1" runat="server" Text ="1"></asp:TextBox>
+     <br>
+        <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication4/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            Label1.Text = Label1.Text + "Ahoj ";
+
+
+
+            SqlConnection conn = new SqlConnection("Data Source=SQL5027.myWindowsHosting.com;Initial Catalog=DB_A19159_macos15;User Id=DB_A19159_macos15_admin;Password=mo22032203;");
+            conn.Open();
+
+            SqlCommand command = new SqlCommand("Select Jmeno from Test where id="+TextBox1.Text, conn);
+
+            // int result = command.ExecuteNonQuery();
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    Label1.Text = reader[0].ToString();
+                }
+            }
+
+            conn.Close();
+
+
+        }
+    }
+}
