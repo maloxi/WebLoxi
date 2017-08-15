e3cb16bd040a750161950ae0aec323d195602293
XeonTruc <XeonTruc@XEONTRUC-PC>
2017-08-15 22:13:06 +0200

    Prvni

--- WebApplication4/Default.aspx
+++ WebApplication4/Default.aspx
@@ -1,5 +1,7 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication4.Default" %>
 
+<%@ Register Assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" Namespace="System.Web.UI.DataVisualization.Charting" TagPrefix="asp" %>
+
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -12,11 +14,39 @@
         <asp:Label ID="Label1" runat="server" Text="Jmeno"></asp:Label>
        
 
-           <br>
+           <br/>
         <asp:TextBox ID="TextBox1" runat="server" Text ="1"></asp:TextBox>
-     <br>
-        <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+     <br/>
+        <asp:Button ID="Button1" runat="server" Text="cislo" OnClick="Button1_Click" />
+       
+        <asp:Button ID="Button2" runat="server" Text="+1" OnClick="Button2_Click" />
+        <asp:Button ID="Button3" runat="server" Text="-1" OnClick="Button3_Click" />
+          <br/>
+        <hr />
+          <br/>
+        <asp:TextBox ID="TextBox2" runat="server" Width="381px"></asp:TextBox>
+        
+        <asp:Button ID="Button4" runat="server" Text="Odeslat SMS" OnClick="Button4_Click" />
+          <br/>
+        <br/>
+            <hr />
+        <asp:Label ID="Label2" runat="server" Text="IP" ForeColor="White"></asp:Label>
+         <br/>
+        <hr />
+          <hr />
+  <%--      <asp:Chart ID="Chart1" runat="server" DataSourceID="SqlDataSource1">
+            <Series>
+                <asp:Series Name="Series1" ChartType="Bar" XValueMember="Jmeno" YValueMembers="ID"></asp:Series>
+            </Series>
+            <ChartAreas>
+                <asp:ChartArea Name="ChartArea1"></asp:ChartArea>
+            </ChartAreas>
+        </asp:Chart>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:db21ConnectionString %>" SelectCommand="SELECT [ID], [Jmeno] FROM [test]"></asp:SqlDataSource>
+        --%>
     </div>
+       
     </form>
+  
 </body>
 </html>
--- WebApplication4/Default.aspx.cs
+++ WebApplication4/Default.aspx.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -12,20 +15,80 @@ namespace WebApplication4
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            DotazNaIP();
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
 
-            Label1.Text = Label1.Text + "Ahoj ";
+
+            int num;
+            bool jeCislo = int.TryParse(TextBox1.Text, out num);
+            if (jeCislo)
+            {
+                            
+                
+                Dotaz();
+            }
+
+
+            else { Label1.Text = "nejni cislo"; }
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+
+            int num;
+            bool jeCislo = int.TryParse(TextBox1.Text, out num);
+            if (jeCislo)
+            {
+
+                num++;
+                TextBox1.Text = num.ToString();
+
+
+                Dotaz();
+            }
+            else { Label1.Text = "nejni cislo"; }
+
+
+        } 
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {   int num;
+            bool jeCislo = int.TryParse(TextBox1.Text, out num);
+            if (jeCislo)
+            {
+               
+                num--;
+                TextBox1.Text = num.ToString();
+
+
+                Dotaz();
+            }
+            else { Label1.Text = "nejni cislo"; }
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+
+
+            Email();
 
 
 
-            SqlConnection conn = new SqlConnection("Data Source=SQL5027.myWindowsHosting.com;Initial Catalog=DB_A19159_macos15;User Id=DB_A19159_macos15_admin;Password=mo22032203;");
+        }
+
+        void Dotaz()
+        {
+
+
+            SqlConnection conn = new SqlConnection("server=s1.aspify.com;uid=db21;pwd=Salomon1;database=db21");
             conn.Open();
 
-            SqlCommand command = new SqlCommand("Select Jmeno from Test where id="+TextBox1.Text, conn);
+            SqlCommand command = new SqlCommand("Select Jmeno from dbo.test where ID=" + TextBox1.Text, conn);
 
             // int result = command.ExecuteNonQuery();
             using (SqlDataReader reader = command.ExecuteReader())
@@ -34,11 +97,72 @@ namespace WebApplication4
                 {
                     Label1.Text = reader[0].ToString();
                 }
+                else
+                {
+                    Label1.Text = "necte";
+                }
             }
 
             conn.Close();
 
+        }
+
+        void Email(/*string textZpravy, int telCislo*/)
+        {
+            try
+            {
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.To.Add("00420720036407@sms.cz.o2.com");
+                mailMessage.From = new MailAddress("loxi@loxi.cz","LoxiWeb");
+                mailMessage.Subject = "loxiSMS";
+                mailMessage.Body = TextBox2.Text;
+                SmtpClient smtpClient = new SmtpClient("m1.aspify.com");
+               smtpClient.Credentials= new System.Net.NetworkCredential("loxi@loxi.cz", "Salomon1");
+           //  smtpClient.EnableSsl = true; --- nejedna se o zabezpecene spojeni
+           
+                smtpClient.Send(mailMessage);
+
+                TextBox2.Text = "";
+                Response.Write("Email has been sent successfully.");
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Could not send the e-mail - error: " + ex.Message);
+            }
+
+
+
+            
 
         }
+
+        void DotazNaIP()
+        {
+
+
+            SqlConnection conn = new SqlConnection("server=s1.aspify.com;uid=db21;pwd=Salomon1;database=db21");
+            conn.Open();
+
+            SqlCommand command = new SqlCommand("select top (1) IP from dbo.IPRpi order by ID desc", conn);
+
+            // int result = command.ExecuteNonQuery();
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    Label2.Text = reader[0].ToString();
+                }
+                else
+                {
+                    Label2.Text = "necte";
+                }
+            }
+
+            conn.Close();
+
+        }
+
+
     }
 }
